4eeaf3baa31a93dd10c77038ade650cc1c897feb
theMaelstro <maelthedev@gmail.com>
2024-09-07 01:28:24 +0200

    Updated Account Bind Credentials command. - Removed parameters from command. - Added Modal view with input form.

--- cogs/account_bind_credentials.py
+++ cogs/account_bind_credentials.py
@@ -17,6 +17,156 @@ from core.exceptions import (
 from core import BaseCog
 from core.crypto import check_password
 
+async def m_bind_credentials(
+        interaction: discord.Interaction,
+        user_builder: UserBuilder,
+        discord_builder: DiscordBuilder,
+        username: str,
+        password: str
+    ):
+    """Bind user account by using ingame credentials."""
+    # Create session
+    async_session = async_sessionmaker(CONN.engine, expire_on_commit=False)
+    async with async_session() as session:
+        try:
+            # Get user info.
+            user = await user_builder.select_user_by_username(
+                session,
+                username
+            )
+            if user is None:
+                raise UsernameIncorrect(
+                    'Username is incorrect.'
+                )
+
+            if await check_password(password, user.password) is False:
+                raise UnmatchingPasswords(
+                    'Passwords do not match.'
+                )
+
+            discord_id = await discord_builder.check_id(
+                session,
+                str(interaction.user.id)
+            )
+            if discord_id is not None:
+                # Update User
+                if not await discord_builder.bind_user_old(
+                    session,
+                    user.id,
+                    str(interaction.user.id)
+                ):
+                    raise CoroutineFailed(
+                        "Could not update table."
+                    )
+
+                logging.info("%s: %s", interaction.user.id, "Account Updated")
+                await interaction.response.send_message(
+                    embed=discord.Embed(
+                        title="Account Updated",
+                        color=discord.Color.green()
+                    ),
+                    ephemeral=True
+                )
+            else:
+                # Register User
+                await discord_builder.bind_user_new(
+                    session,
+                    user.id,
+                    str(interaction.user.id)
+                )
+                logging.info("%s: %s", interaction.user.id, "Account Registered")
+                await interaction.response.send_message(
+                    embed=discord.Embed(
+                        title="Account Registered",
+                        color=discord.Color.green()
+                    ),
+                    ephemeral=True
+                )
+            # Commit
+            await session.commit()
+
+        except (
+            UsernameIncorrect,
+            UnmatchingPasswords
+        ) as e:
+            logging.warning("%s: %s", interaction.user.id, e)
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    title="Binding Failed",
+                    description=e,
+                    color=discord.Color.red()
+                ),
+                ephemeral=True
+            )
+
+        except (
+            CoroutineFailed
+        ) as e:
+            logging.error("%s: %s", interaction.user.id, e)
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    title="Binding Failed",
+                    description=e,
+                    color=discord.Color.red()
+                ),
+                ephemeral=True
+            )
+
+        except Exception as e:
+            logging.error("%s: %s", interaction.user.id, e)
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    title="Binding Failed",
+                    description="Unhandled exception.",
+                    color=discord.Color.red()
+                ),
+                ephemeral=True
+            )
+
+        finally:
+            # Close Session
+            await session.close()
+
+class ModalBindCredentials(
+    discord.ui.Modal,
+    title='Bind Credentials'
+):
+    def __init__(
+        self,
+        user_builder: UserBuilder,
+        discord_builder: DiscordBuilder
+    ):
+        super().__init__()
+        self.user_builder = user_builder
+        self.discord_builder = discord_builder
+    username = discord.ui.TextInput(
+        label='Username',
+        placeholder='Type in your username...',
+    )
+    password = discord.ui.TextInput(
+        label='Password (encrypted in the process)',
+        placeholder='Type in your password...',
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await m_bind_credentials(
+            interaction,
+            self.user_builder,
+            self.discord_builder,
+            self.username.value,
+            self.password.value
+        )
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+        logging.error("%s: %s %s %s", interaction.user.id, type(error), error, error.__traceback__)
+        await interaction.response.send_message(
+            embed=discord.Embed(
+                title="Binding Failed",
+                color=discord.Color.red()
+            ),
+            ephemeral=True
+        )
+
 class AccountBindCredentials(BaseCog):
     """Cog handling binding account with credentials."""
     def __init__(self, client: commands.Bot):
@@ -36,111 +186,10 @@ class AccountBindCredentials(BaseCog):
     async def account_bind_credentials(
         self,
         interaction: discord.Interaction,
-        username: str,
-        password: str
     ):
-        """Bind user account by using ingame credentials."""
-        # Create session
-        async_session = async_sessionmaker(CONN.engine, expire_on_commit=False)
-        async with async_session() as session:
-            try:
-                # Get user info.
-                user = await self.user_builder.select_user_by_username(
-                    session,
-                    username
-                )
-                if user is None:
-                    raise UsernameIncorrect(
-                        'Username is incorrect.'
-                    )
-
-                if await check_password(password, user.password) is False:
-                    raise UnmatchingPasswords(
-                        'Passwords do not match.'
-                    )
-
-                discord_id = await self.discord_builder.check_id(
-                    session,
-                    str(interaction.user.id)
-                )
-                if discord_id is not None:
-                    # Update User
-                    if not await self.discord_builder.bind_user_old(
-                        session,
-                        user.id,
-                        str(interaction.user.id)
-                    ):
-                        raise CoroutineFailed(
-                            "Could not update table."
-                        )
-
-                    logging.info("%s: %s", interaction.user.id, "Account Updated")
-                    await interaction.response.send_message(
-                        embed=discord.Embed(
-                            title="Account Updated",
-                            color=discord.Color.green()
-                        ),
-                        ephemeral=True
-                    )
-                else:
-                    # Register User
-                    await self.discord_builder.bind_user_new(
-                        session,
-                        user.id,
-                        str(interaction.user.id)
-                    )
-                    logging.info("%s: %s", interaction.user.id, "Account Registered")
-                    await interaction.response.send_message(
-                        embed=discord.Embed(
-                            title="Account Registered",
-                            color=discord.Color.green()
-                        ),
-                        ephemeral=True
-                    )
-                # Commit
-                await session.commit()
-
-            except (
-                UsernameIncorrect,
-                UnmatchingPasswords
-            ) as e:
-                logging.warning("%s: %s", interaction.user.id, e)
-                await interaction.response.send_message(
-                    embed=discord.Embed(
-                        title="Binding Failed",
-                        description=e,
-                        color=discord.Color.red()
-                    ),
-                    ephemeral=True
-                )
-
-            except (
-                CoroutineFailed
-            ) as e:
-                logging.error("%s: %s", interaction.user.id, e)
-                await interaction.response.send_message(
-                    embed=discord.Embed(
-                        title="Binding Failed",
-                        description=e,
-                        color=discord.Color.red()
-                    ),
-                    ephemeral=True
-                )
-
-            except Exception as e:
-                logging.error("%s: %s", interaction.user.id, e)
-                await interaction.response.send_message(
-                    embed=discord.Embed(
-                        title="Binding Failed",
-                        description="Unhandled exception.",
-                        color=discord.Color.red()
-                    ),
-                    ephemeral=True
-                )
-
-            finally:
-                # Close Session
-                await session.close()
+        await interaction.response.send_modal(
+            ModalBindCredentials(self.user_builder, self.discord_builder)
+        )
 
     @account_bind_credentials.error
     async def on_account_bind_credentials_error(
