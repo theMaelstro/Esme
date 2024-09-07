5f14cd2649cf30bfb9b524e30adc89fea1d4eb57
theMaelstro <maelthedev@gmail.com>
2024-09-07 02:47:53 +0200

    Updated Token Reset command. - Removed parameters from command. - Added Modal view with input form. - Linting.

--- cogs/account_token_reset.py
+++ cogs/account_token_reset.py
@@ -18,6 +18,128 @@ from core.exceptions import (
 from core import BaseCog
 from core.crypto import check_password
 
+async def m_token_reset(
+        interaction: discord.Interaction,
+        user_builder: UserBuilder,
+        discord_builder: DiscordBuilder,
+        username: str,
+        password: str
+):
+    """Reset account token."""
+    # Create session
+    async_session = async_sessionmaker(CONN.engine, expire_on_commit=False)
+    async with async_session() as session:
+        try:
+            user = await user_builder.select_user_by_username(session, username)
+            if user is None:
+                raise UsernameIncorrect(
+                    'Username is incorrect.'
+                )
+
+            if await check_password(password, user.password) is False:
+                raise UnmatchingPasswords(
+                    'Passwords do not match.'
+                )
+
+            discord_id = await discord_builder.check_id(session, str(interaction.user.id))
+            if discord_id is None:
+                raise DiscordNotRegistered(
+                    f'{interaction.user.mention} account is not registered.'
+                )
+
+            if not await user_builder.clear_user_token(
+                session,
+                user.id
+            ):
+                raise CoroutineFailed(
+                    "Could not update table."
+                )
+            logging.info("%s: %s", interaction.user.id, "Token Reset")
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    title="Token Reset Success",
+                    description="To generate new token use `!discord` command in game.",
+                    color=discord.Color.green()
+                ),
+                ephemeral=True
+            )
+
+            # Commit
+            await session.commit()
+
+        except (
+            UsernameIncorrect,
+            UnmatchingPasswords,
+            UnmatchingPasswords
+        ) as e:
+            logging.warning("%s: %s", interaction.user.id, e)
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    title="Token Reset Failed",
+                    description=e,
+                    color=discord.Color.red()
+                ),
+                ephemeral=True
+            )
+
+        except (
+            CoroutineFailed
+        ) as e:
+            logging.error("%s: %s", interaction.user.id, e)
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    title="Token Reset Failed",
+                    description=e,
+                    color=discord.Color.red()
+                ),
+                ephemeral=True
+            )
+
+        finally:
+            # Close Session
+            await session.close()
+
+class ModalTokenReset(
+    discord.ui.Modal,
+    title='Token Reset'
+):
+    """Discord Modal view class."""
+    def __init__(
+        self,
+        user_builder: UserBuilder,
+        discord_builder: DiscordBuilder
+    ):
+        super().__init__()
+        self.user_builder = user_builder
+        self.discord_builder = discord_builder
+    username = discord.ui.TextInput(
+        label='Username',
+        placeholder='Type in your username...',
+    )
+    password = discord.ui.TextInput(
+        label='Password (encrypted in the process)',
+        placeholder='Type in your password...',
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await m_token_reset(
+            interaction,
+            self.user_builder,
+            self.discord_builder,
+            self.username.value,
+            self.password.value
+        )
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+        logging.error("%s: %s %s %s", interaction.user.id, type(error), error, error.__traceback__)
+        await interaction.response.send_message(
+            embed=discord.Embed(
+                title="Token Reset Failed",
+                color=discord.Color.red()
+            ),
+            ephemeral=True
+        )
+
 class AccountTokenReset(BaseCog):
     """Cog handling token reset with credentials."""
     def __init__(self, client: commands.Bot):
@@ -36,83 +158,12 @@ class AccountTokenReset(BaseCog):
     )
     async def account_token_reset(
         self,
-        interaction: discord.Interaction,
-        username: str,
-        password: str
+        interaction: discord.Interaction
     ):
         """Reset account token."""
-        # Create session
-        async_session = async_sessionmaker(CONN.engine, expire_on_commit=False)
-        async with async_session() as session:
-            try:
-                user = await self.user_builder.select_user_by_username(session, username)
-                if user is None:
-                    raise UsernameIncorrect(
-                        'Username is incorrect.'
-                    )
-
-                if await check_password(password, user.password) is False:
-                    raise UnmatchingPasswords(
-                        'Passwords do not match.'
-                    )
-
-                discord_id = await self.discord_builder.check_id(session, str(interaction.user.id))
-                if discord_id is None:
-                    raise DiscordNotRegistered(
-                        f'{interaction.user.mention} account is not registered.'
-                    )
-
-                if not await self.user_builder.clear_user_token(
-                    session,
-                    user.id
-                ):
-                    raise CoroutineFailed(
-                        "Could not update table."
-                    )
-                logging.info("%s: %s", interaction.user.id, "Token Reset")
-                await interaction.response.send_message(
-                    embed=discord.Embed(
-                        title="Token Reset",
-                        description="To generate new token use `!discord` command in game.",
-                        color=discord.Color.green()
-                    ),
-                    ephemeral=True
-                )
-
-                # Commit
-                await session.commit()
-
-            except (
-                UsernameIncorrect,
-                UnmatchingPasswords,
-                UnmatchingPasswords
-            ) as e:
-                logging.warning("%s: %s", interaction.user.id, e)
-                await interaction.response.send_message(
-                    embed=discord.Embed(
-                        title="Token Reset Failed",
-                        description=e,
-                        color=discord.Color.red()
-                    ),
-                    ephemeral=True
-                )
-
-            except (
-                CoroutineFailed
-            ) as e:
-                logging.error("%s: %s", interaction.user.id, e)
-                await interaction.response.send_message(
-                    embed=discord.Embed(
-                        title="Token Reset Failed",
-                        description=e,
-                        color=discord.Color.red()
-                    ),
-                    ephemeral=True
-                )
-
-            finally:
-                # Close Session
-                await session.close()
+        await interaction.response.send_modal(
+            ModalTokenReset(self.user_builder, self.discord_builder)
+        )
 
     @account_token_reset.error
     async def on_account_token_reset_error(
