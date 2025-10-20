88f090e74e41a87f0b3142b863eb2f481c3629f8
theMaelstro <maelthedev@gmail.com>
2025-10-20 19:21:06 +0200

    Festi start time fix. - Fixed incorrect festi start time (offset was added twice)

--- cogs/async_listener.py
+++ cogs/async_listener.py
@@ -210,10 +210,7 @@ class AsyncListener(BaseCog):
                 )
             payload =json.loads(notification.payload)
             if payload['event_type'] == 'festa':
-                utc_time_now = (
-                    datetime.now(timezone.utc)
-                    + timedelta(hours=CONFIG.erupe.timestamp_offset)
-                )
+                utc_time_now = datetime.now(timezone.utc)
                 events = {
                     "Festi Registration Week": await guild.create_scheduled_event(
                         name = f"Hunter Festival #{payload['id']}: Registration",
